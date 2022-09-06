1a94e19ceaf1c4e19e0f1b59b799969181ae87e1
Débora Christyna <cdias.proj@gmail.com>
2022-09-05 23:11:28 -0300

    acrescenta no estudo visualização de dados

---
+++ intro-pyhton-linkedin/loopFor.py
@@ -0,0 +1,13 @@
+def loopFor():
+    for x in range(5,10):
+        print(x)
+        
+loopFor()
+
+# Usando LOOP FOR em uma coleção
+def loopArray ():
+    dias = ['seg'. 'ter', 'qua', 'qui', 'sex', 'sab', 'dom']
+    for d in dias:
+        print(d)
+
+loopArray()
---
+++ intro-pyhton-linkedin/loopWhile.py
@@ -0,0 +1,7 @@
+def loopWhile():
+    x = 0
+    while (x < 5):
+        print(x)
+        x = x + 1 #incremento necessário para não printar infinitamente o zero definido na linha 2 como valor de x
+
+loopWhile()
