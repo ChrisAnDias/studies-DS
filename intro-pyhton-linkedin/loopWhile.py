29a18eae87c8b347c8dfc765b3c7b5d9d9eca3c2
Débora Christyna <cdias.proj@gmail.com>
2022-09-08 21:44:01 -0300

    deleta arquivos curso pelo linkedin

--- intro-pyhton-linkedin/loopWhile.py
+++
@@ -1,7 +0,0 @@
-def loopWhile():
-    x = 0
-    while (x < 5):
-        print(x)
-        x = x + 1 #incremento necessário para não printar infinitamente o zero definido na linha 2 como valor de x
-
-loopWhile()
